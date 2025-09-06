6b92243ad748a818b87e1c5cc0a34c5126539036
Lucas <thematrixbunny@proton.me>
2025-09-06 17:42:29 -0300

    tests on make_move function

--- test/ex_mon_test.exs
+++ test/ex_mon_test.exs
@@ -31,4 +31,21 @@ defmodule ExMonTest do
       assert messages =~ "turn: :player"
     end
   end
+
+  describe "make_move/1" do
+    test "when the move is valid, do the move and the cmputer makes a move" do
+      player = Player.build("Lucas", :chute, :soco, :cura)
+      ExMon.start_game(player)
+
+      messages =
+        capture_io(fn ->
+          ExMon.make_move(:chute)
+        end)
+
+      assert messages =~ "The Player attacked the computer"
+      assert messages =~ "It's computer turn"
+      assert messages =~ "It's player turn"
+      assert messages =~ "status: :continue"
+    end
+  end
 end
